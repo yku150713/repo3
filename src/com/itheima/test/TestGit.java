d8eebcdc38c3a1422b738990e7c2867240122065
yaoguang <766232863@qq.com>
2018-10-15 00:36:54 +0800

    把本地工程退驾到git

--- src/com/itheima/test/TestGit.java
+++ src/com/itheima/test/TestGit.java
@@ -3,5 +3,12 @@ package com.itheima.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("zhixingla............");
+        //java工程
+    }
+    public void test(){
+        int i=0;
+        i++;
+        System.out.println(i);
+
     }
 }
